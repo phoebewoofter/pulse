3bfc91adc5214e0aa5cd984102c348f3d7ff284f
pwoofter <phoebewoofter@gmail.com>
2024-08-30 14:37:28 -0700

    Configuring authentication page (ie log in)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const CLIENT_ID = "84b572100dda404a8debfc4a94bda0f4";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const REPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token").split("=")[1]);
+      
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+      setToken(token);
+    }
+
+  }, []);
+   
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>pulse</h1>
+        {!token ?
+         <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${REPONSE_TYPE}`}>Log in to Spotify</a>
+        : <button onClick={logout}>Log out</button>}
       </header>
-    </div>
+    </>
   );
 }
 
